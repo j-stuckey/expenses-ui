6ba257f907d3c5fe3381d919c68094536e36d3db
James Stuckey <jstuckey@rutgers.edu>
2019-05-27 22:42:44 -0400

    Added state for controlled form

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import styles from './styles/LandingPage.module.css';
 
 export class LandingPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+		
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+	
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log({ submitted: this.state });
+    }
+
     render() {
         return (
             <React.Fragment>
                 <div className={styles.Container}>
-                    <Link to="/login" className={styles.LoginButton}>Login</Link>
+                    <Link to="/login" className={styles.LoginButton}>
+                        Login
+                    </Link>
 
-                    <form className={styles.Form}>
+                    <form className={styles.Form} onSubmit={this.handleSubmit}>
                         <legend className={styles.FormLegend}>
-              Sign for a free account
+                            Sign for a free account
                         </legend>
                         <div className={styles.Names}>
                             <label htmlFor="firstName" />
@@ -20,7 +39,10 @@ export class LandingPage extends Component {
                                 type="text"
                                 name="firstName"
                                 placeholder="First name"
-                                className={`${styles.Input} ${styles.LeftInput}`}
+                                className={`${styles.Input} ${
+                                    styles.LeftInput
+                                }`}
+                                onChange={this.handleChange}
                             />
 
                             <label htmlFor="firstName" />
@@ -28,7 +50,10 @@ export class LandingPage extends Component {
                                 type="text"
                                 name="lastName"
                                 placeholder="Last name"
-                                className={`${styles.Input} ${styles.RightInput}`}
+                                className={`${styles.Input} ${
+                                    styles.RightInput
+                                }`}
+                                onChange={this.handleChange}
                                 required
                             />
                         </div>
@@ -38,26 +63,31 @@ export class LandingPage extends Component {
                             <input
                                 type="email"
                                 name="email"
-                                className={`${styles.Input} ${styles.LeftInput}`}
+                                className={`${styles.Input} ${
+                                    styles.LeftInput
+                                }`}
                                 placeholder="Email"
+                                onChange={this.handleChange}
                                 required
                             />
                             <label htmlFor="password" />
                             <input
                                 type="password"
-                                className={`${styles.Input} ${styles.RightInput}`}
+                                className={`${styles.Input} ${
+                                    styles.RightInput
+                                }`}
                                 name="password"
                                 placeholder="Password"
+                                onChange={this.handleChange}
                                 required
                             />
                         </div>
-                        <button className={styles.SubmitButton}>Create account</button>
+                        <button className={styles.SubmitButton}>
+                            Create account
+                        </button>
                     </form>
-
                 </div>
-                <div className={styles.Bottom}>
-					
-                </div>
+                <div className={styles.Bottom} />
             </React.Fragment>
         );
     }
